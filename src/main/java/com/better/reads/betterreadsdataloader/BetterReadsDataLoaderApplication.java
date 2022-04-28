32f177f72c1b32668c2d2a63eea0698bbec1406c
paul.rus <paul.rus@globant.com>
2022-04-28 12:01:48 +0300

    Create project

---
+++ src/main/java/com/better/reads/betterreadsdataloader/BetterReadsDataLoaderApplication.java
@@ -0,0 +1,14 @@
+package com.better.reads.betterreadsdataloader;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetterReadsDataLoaderApplication {
+
+	public static void main(String[] args) {
+		System.out.println("helloo");
+		SpringApplication.run(BetterReadsDataLoaderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/better/reads/betterreadsdataloader/BetterReadsDataLoaderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.better.reads.betterreadsdataloader;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetterReadsDataLoaderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
