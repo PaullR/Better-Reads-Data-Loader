e46689fb63e2ec64bd55a6370a48b6852f8e0996
paul.rus <paul.rus@globant.com>
2022-04-28 18:43:29 +0300

    Connect to Cassandra hosted by DataStax

--- src/main/java/com/better/reads/betterreadsdataloader/BetterReadsDataLoaderApplication.java
+++ src/main/java/com/better/reads/betterreadsdataloader/BetterReadsDataLoaderApplication.java
@@ -1,14 +1,38 @@
 package com.better.reads.betterreadsdataloader;
 
+import com.better.reads.betterreadsdataloader.connection.DataStaxAstraProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.cassandra.CqlSessionBuilderCustomizer;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.context.annotation.Bean;
+
+import java.nio.file.Path;
 
 @SpringBootApplication
+@EnableConfigurationProperties(DataStaxAstraProperties.class)
 public class BetterReadsDataLoaderApplication {
 
 	public static void main(String[] args) {
-		System.out.println("helloo");
 		SpringApplication.run(BetterReadsDataLoaderApplication.class, args);
 	}
 
+	/**
+	 * This is necessary to have the Spring Boot app use the Astra secure bundle
+	 * to connect to the database
+	 */
+//	@Bean
+//	public CqlSessionBuilderCustomizer sessionBuilderCustomizer(DataStaxAstraProperties astraProperties) {
+//		Path bundle = astraProperties.getSecureConnectBundle().toPath();
+//		return builder -> builder.withCloudSecureConnectBundle(bundle);
+//	}
+
+
+	@Bean
+	public String getYmlProperties(DataStaxAstraProperties astraProperties) {
+		String test = astraProperties.getTest();
+
+		System.out.println("test");
+		return test;
+	}
 }
